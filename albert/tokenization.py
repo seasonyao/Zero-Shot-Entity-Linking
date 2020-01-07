4df04ecc1dee6b3b4ccf6e44241ef4f49512ffeb
姚宗海 <yaozonghai@yaozonghaideMacBook-Pro.local>
2020-01-07 12:29:17 -0500

    change tokenization

--- albert/tokenization.py
+++ albert/tokenization.py
@@ -236,7 +236,7 @@ class FullTokenizer(object):
     self.sp_model = None
     if spm_model_file:
       self.sp_model = spm.SentencePieceProcessor()
-      tf.logging.info("loading sentence piece model")
+      #tf.logging.info("loading sentence piece model")
       self.sp_model.Load(spm_model_file)
       # Note(mingdachen): For the purpose of consisent API, we are
       # generating a vocabulary for the sentence piece tokenizer.
@@ -261,7 +261,7 @@ class FullTokenizer(object):
 
   def convert_tokens_to_ids(self, tokens):
     if self.sp_model:
-      tf.logging.info("using sentence piece tokenzier.")
+      #tf.logging.info("using sentence piece tokenzier.")
       return [self.sp_model.PieceToId(
           printable_text(token)) for token in tokens]
     else:
@@ -269,7 +269,7 @@ class FullTokenizer(object):
 
   def convert_ids_to_tokens(self, ids):
     if self.sp_model:
-      tf.logging.info("using sentence piece tokenzier.")
+      #tf.logging.info("using sentence piece tokenzier.")
       return [self.sp_model.IdToPiece(id_) for id_ in ids]
     else:
       return convert_by_vocab(self.inv_vocab, ids)
